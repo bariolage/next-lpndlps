03a79a3a1a672bb932916bc134332b5066ee5227
Ju <40365206+bariolage@users.noreply.github.com>
2021-02-12 15:28:22 +0100

    clean

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,11 @@ import "../styles/globals.css";
 import Page from "../components/page";
 import matter from "gray-matter";
 import React, { useContext, useState } from "react";
-import "typeface-boogaloo";
-import "typeface-mukta";
-import "typeface-damion";
-import "typeface-cookie";
-import "typeface-cutive-mono";
 import "typeface-dosis";
-import "typeface-pompiere";
+
+export function reportWebVitals(metric) {
+  console.log(metric);
+}
 
 export const Context = React.createContext();
 
@@ -26,6 +24,7 @@ function MyApp({ Component, pageProps }) {
   const [menuIsOpen, toggle] = useState(false);
   const viewportInit = { lat: 48.35261871558314, lng: -4.4191839176185725 };
   const [viewport, setViewport] = useState(viewportInit);
+
   return (
     <Context.Provider
       value={{
@@ -36,23 +35,21 @@ function MyApp({ Component, pageProps }) {
         viewportInit,
       }}
     >
-      <Page {...pageProps}>
-        <Component {...pageProps} />
-      </Page>
+      <Component {...pageProps} />
     </Context.Provider>
   );
 }
 
-MyApp.getInitialProps = async () => {
-  const infos = await import(`../content/infos.json`);
-  const content = await import(`../content/pages/contact.md`);
-  const data = matter(content.default);
-  return {
-    pageProps: {
-      infos: infos.default,
-      contactData: { frontmatter: data.data, content: data.content },
-    },
-  };
-};
+// MyApp.getInitialProps = async () => {
+//   const infos = await import(`../content/infos.json`);
+//   const content = await import(`../content/pages/contact.md`);
+//   const data = matter(content.default);
+//   return {
+//     pageProps: {
+//       infos: infos.default,
+//       contactData: { frontmatter: data.data, content: data.content },
+//     },
+//   };
+// };
 
 export default MyApp;
