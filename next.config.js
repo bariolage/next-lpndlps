0251a6211eb96c1d37bfab31e5356d55639011f2
Ju <40365206+bariolage@users.noreply.github.com>
2021-02-17 15:03:51 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,6 +10,28 @@ const nextConfig = {
     });
     return config;
   },
+  images: {
+    imageSizes: [],
+    deviceSizes: [
+      16,
+      32,
+      48,
+      64,
+      96,
+      128,
+      256,
+      384,
+      512,
+      640,
+      750,
+      828,
+      1080,
+      1200,
+      1920,
+      2048,
+      3840,
+    ],
+  },
   pwa: {
     dest: "public",
     register: true,
