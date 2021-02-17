e108272de5ff371ad57fc55241bc7cc885fd35e2
Ju <40365206+bariolage@users.noreply.github.com>
2021-02-17 15:09:38 +0100

    Update seo.js

--- components/seo.js
+++ components/seo.js
@@ -37,14 +37,14 @@ export default function SEO({ globalData, title }) {
       <Head>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=0.86, maximum-scale=5.0, minimum-scale=0.86"
-        />
         {/* <meta
           name="viewport"
-          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+          content="width=device-width, initial-scale=0.86, maximum-scale=5.0, minimum-scale=0.86"
         /> */}
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
         <meta name="description" content={globalData.description} />
         <title>{seo.title}</title>
         <link rel="canonical" href={canURL} />
