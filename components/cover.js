017cd1b988d70b03fd6451b4e8655dd38feb65dd
Ju <40365206+bariolage@users.noreply.github.com>
2021-04-15 17:26:13 +0200

    maj

--- components/cover.js
+++ components/cover.js
@@ -2,31 +2,36 @@ import Image from "next/image";
 import styled from "styled-components";
 
 const Wrap = styled.section`
-  width: 50%;
+ width: 50%;
+ padding: 0rem;
+ @media (max-width: 48rem) {
+  width: 100%;
   padding: 0rem;
-  @media (max-width: 48rem) {
-    width: 100%;
-    padding: 0rem;
-    height: 20rem;
-  }
+  height: 20rem;
+ }
 `;
 
 const Figure = styled.figure`
-  position: relative;
-  margin: 0;
-  background-color: lighten(blue, 15%);
-  background-blend-mode: multiply;
-  mix-blend-mode: multiply;
-  width: 100%;
-  height: 100%;
+ position: relative;
+ margin: 0;
+ background-color: lighten(blue, 15%);
+ background-blend-mode: multiply;
+ mix-blend-mode: multiply;
+ width: 100%;
+ height: 100%;
 `;
 
 export default function Cover({ image }) {
-  return (
-    <Wrap>
-      <Figure>
-        <Image alt={image?.alt} src={image} layout="fill" objectFit="cover" />
-      </Figure>
-    </Wrap>
-  );
+ return (
+  <Wrap>
+   <Figure>
+    <Image
+     alt={image.alt || "le pain des lou - illustration"}
+     src={image}
+     layout="fill"
+     objectFit="cover"
+    />
+   </Figure>
+  </Wrap>
+ );
 }
--- components/seo.js
+++ components/seo.js
@@ -2,86 +2,84 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 
 export default function SEO({ globalData, title }) {
-  const canURL = globalData.global.url + useRouter().pathname;
+ const canURL = globalData.global.url + useRouter().pathname;
 
-  const seo = {
-    title: globalData.global.title + " - " + title || globalData.global.title,
-    image: globalData.contact.banner.url,
-    description: globalData.global.description,
-    url: canURL,
-  };
+ const seo = {
+  title: globalData.global.title + " - " + title || globalData.global.title,
+  image: globalData.contact.banner.url,
+  description: globalData.global.description,
+  url: canURL,
+ };
 
-  const schema = [
-    {
-      "@context": "http://schema.org/",
-      "@type": "Bakery",
-      name: globalData.global.title,
-      description: globalData.global.description,
-      image: globalData.contact.banner.url,
-      url: globalData.global.url,
-      telephone: globalData.contact.telephone[0],
-      address: {
-        "@type": "PostalAddress",
-        streetAddress: globalData.contact.address.streetAddress,
-        addressLocality: globalData.contact.address.addressLocality,
-        postalCode: globalData.contact.address.postalCode,
-        addressCountry: globalData.contact.address.addressCountry,
-      },
-    },
-  ];
+ const schema = {
+  "@context": "http://schema.org/",
+  "@type": "Bakery",
+  name: globalData.global.title,
+  description: globalData.global.description,
+  image: globalData.contact.banner.url,
+  url: globalData.global.url,
+  telephone: globalData.contact.telephone[0].number,
+  address: {
+   "@type": "PostalAddress",
+   streetAddress: globalData.contact.address.streetAddress,
+   addressLocality: globalData.contact.address.addressLocality,
+   postalCode: globalData.contact.address.postalCode,
+   addressCountry: globalData.contact.address.addressCountry,
+  },
+ };
 
-  const jsonSchema = JSON.stringify(schema);
+ const jsonSchema = JSON.stringify(schema);
 
-  return (
-    <>
-      <Head>
-        {/* <meta charSet="utf-8" /> */}
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        {/* <meta
+ return (
+  <>
+   <Head>
+    {/* <meta charSet="utf-8" /> */}
+    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+    {/* <meta
           name="viewport"
           content="width=device-width, initial-scale=0.86, maximum-scale=5.0, minimum-scale=0.86"
         /> */}
-        <meta
-          content="text/html; charset=UTF-8; X-Content-Type-Options=nosniff"
-          httpEquiv="Content-Type"
-        />
-        <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1,maximum-scale=5.0, minimum-scale=1"
-        />
-        <meta name="description" content={globalData.global.description} />
-        <title>{seo.title}</title>
-        <link rel="canonical" href={canURL} />
-        <meta name="image" content={seo.image} />
-        <link rel="manifest" href="/manifest.json" />
-        <link
-          href="/icons/favicon-16x16.png"
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-        />
-        <link
-          href="/icons/favicon-32x32.png"
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-        />
-        <link rel="apple-touch-icon" href="/icons/icon-96x96.png"></link>
-        <meta name="theme-color" content="#b00a00" />
+    <meta
+     content="text/html; charset=UTF-8; X-Content-Type-Options=nosniff"
+     httpEquiv="Content-Type"
+    />
+    <meta
+     name="viewport"
+     content="width=device-width,initial-scale=1,maximum-scale=5.0, minimum-scale=1"
+    />
+    <meta name="description" content={globalData.global.description} />
+    <title>{seo.title}</title>
+    <link rel="canonical" href={canURL} />
+    <meta name="image" content={seo.image} />
+    <link rel="manifest" href="/manifest.json" />
+    <link
+     href="/icons/favicon-16x16.png"
+     rel="icon"
+     type="image/png"
+     sizes="16x16"
+    />
+    <link
+     href="/icons/favicon-32x32.png"
+     rel="icon"
+     type="image/png"
+     sizes="32x32"
+    />
+    <link rel="apple-touch-icon" href="/icons/icon-96x96.png"></link>
+    <meta name="theme-color" content="#b00a00" />
 
-        {/* OpenGraph tags */}
-        <meta property="og:url" content={seo.url} />
-        <meta property="og:title" content={seo.title} />
-        <meta property="og:description" content={seo.description} />
-        <meta property="og:image" content={seo.image} />
+    {/* OpenGraph tags */}
+    <meta property="og:url" content={seo.url} />
+    <meta property="og:title" content={seo.title} />
+    <meta property="og:description" content={seo.description} />
+    <meta property="og:image" content={seo.image} />
 
-        {/* Twitter Card tags */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={seo.title} />
-        <meta name="twitter:description" content={seo.description} />
-        <meta name="twitter:image" content={seo.image} />
-        <script type="application/ld+json">{jsonSchema}</script>
-      </Head>
-    </>
-  );
+    {/* Twitter Card tags */}
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content={seo.title} />
+    <meta name="twitter:description" content={seo.description} />
+    <meta name="twitter:image" content={seo.image} />
+    <script type="application/ld+json">{jsonSchema}</script>
+   </Head>
+  </>
+ );
 }
